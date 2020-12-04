7396c443ad423b7795c0a568333a36526be11439
Thomas Gooding <tgooding@chapman.edu>
2020-12-03 23:25:38 -0800

    Numbers Porject

--- numbers.sh
+++ numbers.sh
@@ -6,9 +6,12 @@ echo 'Please enter a positive integer:'
 read -r X
 N = 1
 while $N -le $X|
+do
 	$N = $[$N+1]
-	if $((N%2)) -eq 0:
+	if $((N%2)) -eq 0
+	then
 		echo "$X even"
-	else:
+	else
 		echo "$X odd"
-
+	fi
+done
