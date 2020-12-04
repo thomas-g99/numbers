a88e85c3457922961db07be9a495eaefdb6a4d65
Thomas Gooding <tgooding@chapman.edu>
2020-12-04 10:29:52 -0800

    Thomas G Numbers

--- numbers.sh
+++ numbers.sh
@@ -1,13 +1,13 @@
-#! bin/bash
+#! /bin/bash
 # numbers.sh
 # Thomas Gooding
 #chmod 755
 echo 'Please enter a positive integer:'
 read -r X
 N=1
-while "$N" -le "$X"
+while [ "$N" -le "$X" ]
 do
-	if $((N%2)) -eq 0
+	if [ $((N%2)) -eq 0 ]
 	then
 		echo "$N even"
 	else
